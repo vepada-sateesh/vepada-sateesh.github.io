aa11fa9ca95c0a23c857361e80ddb9a85091aaa9
vepada-sateesh <sateeshvepadas@gmail.com>
2022-10-26 17:06:34 +0530

    added

--- index.js
+++ index.js
@@ -9,3 +9,25 @@
     }
     update()
 
+
+let btn = document.querySelector(".close-btn>h1>button")
+btn.style.color="red"
+btn.addEventListener("click",data)
+function data(){
+    //console.log("ehlo")
+    document.querySelector("#check").checked=false
+}
+
+const text = document.querySelector(".sec-text")
+const textloadFun=()=>{
+    setTimeout(()=>{
+        text.textContent="Web Developer"
+    },0)
+    setTimeout(()=>{
+        text.textContent="Full Stack Web Developer"
+    },4000)
+
+}
+textloadFun()
+setInterval(textloadFun,8000)
+console.log(document.querySelector("#check").checked)
